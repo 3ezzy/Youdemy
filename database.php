d6a901825c530a074baca8f29708e61f455893f1
Mehdi <khammali26@gmail.com>
2025-01-17 09:33:42 +0100

    add: create database connection script for YoudemyDB

---
+++ database.php
@@ -0,0 +1,24 @@
+<?php 
+
+$servername = "localhost";
+$username = "root"; 
+$password = "123"; 
+$dbname = "YoudemyDB";
+
+try {
+    // Création de la connexion
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Erreur de connexion: " . $e->getMessage();
+}
+?>
+
+
+
+
+
+
+
+
+
